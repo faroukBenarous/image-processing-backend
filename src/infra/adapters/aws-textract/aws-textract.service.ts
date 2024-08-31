daaf5cc9e1696c67c75f59199b3b07f319170cb8
Farouk <farouk@coinrule.com>
2024-08-31 18:20:06 +0100

    error handling + extract all field using aws textract

--- src/infra/adapters/aws-textract/aws-textract.service.ts
+++ src/infra/adapters/aws-textract/aws-textract.service.ts
@@ -1,35 +1,56 @@
-import {Injectable, Logger} from "@nestjs/common";
+import {BadRequestException, Injectable, Logger} from "@nestjs/common";
 import {ImageDetails, ImageProcessingService} from "../../../domain/services/image-processing.service";
-import {AnalyzeIDCommand, TextractClient} from "@aws-sdk/client-textract";
-import {Express} from "express";
 import {FileRequest} from "../../ports/http/dto";
+import * as aws from "aws-sdk";
+import {ConfigService} from "@nestjs/config";
 
 @Injectable()
 export class AwsTextractService implements ImageProcessingService {
     private logger: Logger = new Logger(AwsTextractService.name);
 
-    async extractImageDetails(fileImage: FileRequest): Promise<ImageDetails> {
-        return Promise.resolve( {dateOfBirth: "", expiryDate: "", firstName: "", lastName: ""})
+    constructor(private configService: ConfigService) {
+        const awsKey = configService.getOrThrow('AWS_ACCESS_KEY_ID')
+        const awsSecret = configService.getOrThrow('AWS_SECRET_ACCESS_KEY')
+        const awsRegion = configService.getOrThrow('AWS_REGION')
+        aws.config.update({
+            accessKeyId: awsKey,
+            secretAccessKey: awsSecret,
+            region: awsRegion
+        });
     }
 
-    private async main() {
-        const textractClient = new TextractClient();
+    async extractImageDetails(fileImage: FileRequest): Promise<ImageDetails> {
+        const rawData = await this.extractRawData(fileImage);
+        return Promise.resolve({dateOfBirth: "", expiryDate: "", firstName: "", lastName: ""})
+    }
 
-        const analyseIdDocument = new AnalyzeIDCommand({DocumentPages: [{Bytes: Uint8Array.from([])}]});
+    private async extractRawData(fileImage: FileRequest) {
+        try {
 
-        // Textract returns a list of blocks. A block can be a line, a page, word, etc.
-        // Each block also contains geometry of the detected text.
-        // For more information on the Block type, see https://docs.aws.amazon.com/textract/latest/dg/API_Block.html.
-        const response = await textractClient.send(analyseIdDocument);
+            const fileBytes = new Uint8Array(fileImage.buffer.buffer);
+            const params = {DocumentPages: [{Bytes: fileBytes}]}
+
+            const textract = new aws.Textract();
+            const request = textract.analyzeID(params)
+            const data = await request.promise();
+            const fields = data.IdentityDocuments[0].IdentityDocumentFields
+            fields.forEach((field) => {
+                this.logger.debug({
+                    fieldKey: field.Type.Text,
+                    accuracy: field.Type.Confidence,
+                    fieldValue: field.ValueDetection.Text,
+                    convertedValue: field.ValueDetection?.NormalizedValue?.Value,
+                    fieldValueAccuracy: field.ValueDetection.Confidence
+                })
+
+            })
 
 
-        this.logger.log({response})
-        // // For the purpose of this example, we are only interested in words.
-        // const extractedWords = Blocks.filter((b) => b.BlockType === "WORD").map(
-        //     (b) => b.Text,
-        // );
-        //
-        // return extractedWords.join(" ");
+        } catch (err) {
+            this.logger.error(`Error processing the image${err.message}`)
+            throw new BadRequestException(`Error processing the image Please try a taking different picture`)
+        }
+
     }
 
 }
